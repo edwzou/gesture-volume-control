747de7e9af5b3dce319f81fe6494c1e6bb8fe1e5
Edward Zou <edwzou@Edwards-MacBook-Air-2.local>
2024-05-27 23:25:44 -0700

    Implemented osascript

--- GestureVolumeControl.py
+++ GestureVolumeControl.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+import osascript
 
 wCam, hCam = 640, 480
 cap = cv2.VideoCapture(1)
@@ -11,6 +12,10 @@ cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.8)
+
+vol = 0
+volBar = 400
+volPer = 0
 
 while True:
     success, img = cap.read()
@@ -28,7 +33,17 @@ while True:
         cv2.circle(img, (cx, cy), 15, (0, 0, 255), cv2.FILLED)
 
         length = math.hypot(x2 - x1, y2 - y1)
-        print(length)
+        vol = np.interp(length, [50, 200], [0, 100])  # map hand range (50 to 200) to volume range (0 to 100)
+        volBar = np.interp(length, [50, 200], [400, 150])
+        volPer = np.interp(length, [50, 200], [0, 100])
+
+        volCommand = f"set volume output volume {int(vol)}"   # set volume
+        osascript.osascript(volCommand)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (0, 0, 0), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
